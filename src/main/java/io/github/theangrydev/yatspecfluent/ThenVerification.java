49a9cf7e897a60e757f0cc1cafed7802ce18d2bf
Liam Williams <liam.williams@zoho.com>
2016-10-14 21:08:07 +0100

    Fix javadoc

--- src/main/java/io/github/theangrydev/yatspecfluent/ThenVerification.java
+++ src/main/java/io/github/theangrydev/yatspecfluent/ThenVerification.java
@@ -1,14 +1,31 @@
+/*
+ * Copyright 2016 Liam Williams <liam.williams@zoho.com>.
+ *
+ * This file is part of yatspec-fluent.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.theangrydev.yatspecfluent;
 
 /**
- * When the {@link #verify(TestResult)} method is invoked, a verification should be made about the {@link TestResult}.
+ * When the {@link #verify(Object)} method is invoked, a verification should be made about the {@link TestResult}.
  *
  * For example, this could mean verifying a HTTP interaction took place.
  *
  * This class should act as a builder for use in {@link FluentTest}.
  *
  * @see <a href="https://en.wikipedia.org/wiki/Builder_pattern#Java_example">The Builder Pattern</a>
- * @param <TestResult> The test result that the {@link #verify(TestResult)} operates on
+ * @param <TestResult> The test result that the {@link #verify(Object)} operates on
  */
 @FunctionalInterface
 public interface ThenVerification<TestResult> {

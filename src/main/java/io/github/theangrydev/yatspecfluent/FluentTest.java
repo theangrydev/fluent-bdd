2c5c1efb8ed69f575e7eb36d2335438c55cbb55f
Liam Williams <liam.williams@zoho.com>
2016-10-14 21:23:22 +0100

    Updating comment

--- src/main/java/io/github/theangrydev/yatspecfluent/FluentTest.java
+++ src/main/java/io/github/theangrydev/yatspecfluent/FluentTest.java
@@ -30,7 +30,7 @@ import static java.lang.String.format;
  *
  * @param <TestResult> The type of test result produced by the {@link When}
  */
-@SuppressWarnings("PMD.TooManyMethods") // This is part of the API design choice
+@SuppressWarnings("PMD.TooManyMethods") // This is part of the API design choices
 public abstract class FluentTest<TestResult> implements WithTestState, WriteOnlyTestItems {
 
     private final TestState state = new TestState();

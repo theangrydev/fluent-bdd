58911e3dfe91df90876d86cdacded14d9069970b
Liam Williams <liam.williams@zoho.com>
2016-11-11 10:30:49 +0000

    Silly bit of test coverage

--- all/src/main/java/io/github/theangrydev/fluentbdd/Documentation.java
+++ all/src/main/java/io/github/theangrydev/fluentbdd/Documentation.java
@@ -20,5 +20,4 @@ package io.github.theangrydev.fluentbdd;
 /**
  * This class only exists so that a javadoc JAR can be generated with links to javadoc of the other modules.
  */
-@SuppressWarnings("unused")
 public class Documentation {}
